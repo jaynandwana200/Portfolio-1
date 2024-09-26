e31938af220f090ee37ffab16c9cd7cb7e5aa5bc
Jay Nandwana <69632001+jaynandwana200@users.noreply.github.com>
2024-09-26 19:14:34 +0530

    Added new Resume
    New resume and demo link of project

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -76,16 +76,16 @@ function Home2() {
                   <AiFillGithub />
                 </a>
               </li>
-              <li className="social-icons">
+              {/* <li className="social-icons">
                 <a
                   href="https://www.geeksforgeeks.org/user/jaynandwana20001/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
                 >
-                  <AiOutlineTwitter />
+                  < />
                 </a>
-              </li>
+              </li> */}
               <li className="social-icons">
                 <a
                   href="https://www.linkedin.com/in/jay-nandwana-india/"
